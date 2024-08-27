c591da894235dd63573b4f7360ff08554ea3eb98
Gloria <galinaperlyna@gmail.com>
2024-08-27 19:40:35 +0200

    hw-js01

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,11 @@
 "use strict";
 
 function makeTransaction(quantity, pricePerDroid) {
-  const makeTransaction = `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`;
-  console.log(makeTransaction);
+  console.log(
+    `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+  );
   return makeTransaction;
 }
-
-const quantity = 5;
-const pricePerDroid = 3000;
-makeTransaction(quantity, pricePerDroid);
+makeTransaction(5, 3000);
 makeTransaction(3, 1000);
 makeTransaction(10, 500);
--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,12 @@
 "use strict";
 
 function getShippingMessage(country, price, deliveryFee) {
-  const getShippingMessage = `Shipping to ${country} will cost ${
-    price + deliveryFee
-  } credits`;
-  console.log(getShippingMessage);
+  console.log(
+    `Shipping to ${country} will cost ${price + deliveryFee} credits`
+  );
   return getShippingMessage;
 }
 
-const country = "Australia";
-const price = 120;
-const deliveryFee = 50;
-getShippingMessage(country, price, deliveryFee);
+getShippingMessage("Australia", 120, 50);
 getShippingMessage("Germany", 80, 20);
 getShippingMessage("Sweden", 100, 20);
